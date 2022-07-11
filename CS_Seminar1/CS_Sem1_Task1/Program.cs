10127bee9c859715fe2ea9f324d5821e182dbc48
Nina <nzagorevskaia@gmail.com>
2022-07-12 00:52:44 +0300

    Edit Task1 - max and min of 2 numbers

--- CS_Seminar1/CS_Sem1_Task1/Program.cs
+++ CS_Seminar1/CS_Sem1_Task1/Program.cs
@@ -11,12 +11,18 @@ int b = Convert.ToInt32(Console.ReadLine()); // вводим число b
 if (a > b)
 {
     int max = a;
+    int min = b;
     Console.Write("max = ");
     Console.WriteLine(max);
+    Console.Write("min = ");
+    Console.Write(min);
 }
 else 
 {
     int max = b;
+    int min = a;
     Console.Write("max = ");
     Console.WriteLine(max);
+    Console.Write("min = ");
+    Console.Write(min);
 }
