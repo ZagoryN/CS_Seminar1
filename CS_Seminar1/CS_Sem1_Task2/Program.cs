0f165e7e2976be838ff2aad0497194ed0f3912ef
Nina <nzagorevskaia@gmail.com>
2022-07-12 00:55:46 +0300

    Edit Task2 - max of 3 numbers

--- CS_Seminar1/CS_Sem1_Task2/Program.cs
+++ CS_Seminar1/CS_Sem1_Task2/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+Console.Write("Введите первое число: ");  // приглашение ко вводу
+int a = Convert.ToInt32(Console.ReadLine()); // вводим число a
+Console.Write("Введите второе число: ");  // приглашение ко вводу
+int b = Convert.ToInt32(Console.ReadLine()); // вводим число b
+Console.Write("Введите третье число: ");  // приглашение ко вводу
+int c = Convert.ToInt32(Console.ReadLine()); // вводим число с
+
+if (a > b && a > c)
+{
+    int max = a;
+    Console.Write("max = ");
+    Console.WriteLine(max);
+}
+if (b > a && b > c)
+{
+    int max = b;
+    Console.Write("max = ");
+    Console.WriteLine(max);
+}
+if (c > a && c > b)
+{
+    int max = c;
+    Console.Write("max = ");
+    Console.WriteLine(max);
+}
