09321a0c8efce0c39885ee3b6b4dc56b818d1af8
Nina <nzagorevskaia@gmail.com>
2022-07-11 23:57:03 +0300

    Add Task1 - max of 2 numbers

---
+++ CS_Seminar1/CS_Sem1_Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, 
+//какое число большее, а какое меньшее.
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+Console.Write("Введите первое число: ");  // приглашение ко вводу
+int a = Convert.ToInt32(Console.ReadLine()); // вводим число a
+Console.Write("Введите второе число: ");  // приглашение ко вводу
+int b = Convert.ToInt32(Console.ReadLine()); // вводим число b
+if (a > b)
+{
+    int max = a;
+    Console.Write("max = ");
+    Console.WriteLine(max);
+}
+else 
+{
+    int max = b;
+    Console.Write("max = ");
+    Console.WriteLine(max);
+}
