2776fea554c0338cb9ca925839df3ec4e6101486
Nina <nzagorevskaia@gmail.com>
2022-07-12 01:33:35 +0300

    Add Task3 - division by 2

--- CS_Seminar1/CS_Sem1_Task3/Program.cs
+++ CS_Seminar1/CS_Sem1_Task3/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным 
+//(делится ли оно на два без остатка).
+//4 -> да
+//-3 -> нет
+//7 -> нет
+Console.Write("Введите число: ");  // приглашение ко вводу
+int a = Convert.ToInt32(Console.ReadLine()); // вводим число a
+int ost = a % 2; // переменная - остаток от деления на 2
+
+if (ost == 0)
+{
+    Console.Write("Делится ли оно на 2 без остатка? ");
+    Console.WriteLine("Да");
+}
+else 
+{
+    Console.Write("Делится ли оно на 2 без остатка? ");
+    Console.WriteLine("Нет");
+}
