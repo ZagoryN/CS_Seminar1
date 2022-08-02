969ce58baf9ac9bc5ec3a57ff6d4e3767d143134
Nina <nzagorevskaia@gmail.com>
2022-08-02 23:58:33 +0300

    Edit CS_Sem4_Task1

--- CS_Seminar4/CS_Sem4_Task1/Program.cs
+++ CS_Seminar4/CS_Sem4_Task1/Program.cs
@@ -3,7 +3,7 @@
 
 Console.Write("Введите число A: ");  // приглашение ко вводу
 int A = Convert.ToInt32(Console.ReadLine()); // ввод числа A
-Console.Write("Введите степень. в которую нужно возвести А: ");  // приглашение ко вводу
+Console.Write("Введите степень, в которую нужно возвести число А: ");  // приглашение ко вводу
 int B = Convert.ToInt32(Console.ReadLine()); // ввод числа B
 int result = 1;
 for (int i=1; i <= B; i++)
