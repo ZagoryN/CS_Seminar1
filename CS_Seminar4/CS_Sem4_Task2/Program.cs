660a6c7facc6193c2ac5b84f4420426dd5bc8790
Nina <nzagorevskaia@gmail.com>
2022-08-03 00:44:35 +0300

    Add CS_Sem4_Task2 Sum_of_digits in positive/negative numbers

---
+++ CS_Seminar4/CS_Sem4_Task2/Program.cs
@@ -0,0 +1,41 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число 
+//и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+Console.Write("Введите число A: ");  // приглашение ко вводу
+int A = Convert.ToInt32(Console.ReadLine()); // ввод числа A
+
+if (A > 0)    //на случай ввода отрицательных чисел int
+{
+    int N = A; //для красивого вывода результата
+
+    double temp = 0;
+    while (A > 10) 
+    {
+        double b = A % 10;
+        A = A / 10;
+        temp = temp + b;   
+    }
+    temp = A + temp;
+    Console.WriteLine($"Сумма цифр в числе {N}: {temp}.");
+}
+else
+{
+    int N = A; //для красивого вывода результата
+    A = Math.Abs(A);  //при отрицательных числах работаем с модулем числа
+
+    double temp = 0;
+    while (A > 10) 
+    {
+        double b = A % 10;
+        A = A / 10;
+        temp = temp + b;   
+    }
+    temp = A + temp;
+    Console.WriteLine($"Сумма цифр в числе {N}: {temp}.");
+}
+//Console.WriteLine(result);
+//Console.WriteLine(temp);
+
+
