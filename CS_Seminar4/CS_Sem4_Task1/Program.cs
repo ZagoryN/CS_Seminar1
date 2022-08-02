67ee4176b2f3740e93e58abd2d87c176e9beaadc
Nina <nzagorevskaia@gmail.com>
2022-08-02 21:29:30 +0300

    Add CS_Sem4_Task1 Возведение в степень

---
+++ CS_Seminar4/CS_Sem4_Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+//  и возводит число A в натуральную степень B.
+
+Console.Write("Введите число A: ");  // приглашение ко вводу
+int A = Convert.ToInt32(Console.ReadLine()); // ввод числа A
+Console.Write("Введите степень. в которую нужно возвести А: ");  // приглашение ко вводу
+int B = Convert.ToInt32(Console.ReadLine()); // ввод числа B
+int result = 1;
+for (int i=1; i <= B; i++)
+{
+    result=result*A;
+}
+Console.Write(result);
