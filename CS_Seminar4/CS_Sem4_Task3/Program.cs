1d59ef3fe39a83e8a246a70e548a54020bb687d2
Nina <nzagorevskaia@gmail.com>
2022-08-05 15:14:01 +0300

    Edit CS_Sem4_Task3 string with result

---
+++ CS_Seminar4/CS_Sem4_Task3/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19] 
+//6, 1, 33 -> [6, 1, 33]
+
+Console.Write("Введите количество элементов в массиве: ");  // приглашение ко вводу
+int n = Convert.ToInt16(Console.ReadLine());
+int[] array = new int[n];
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write("Введите элемент массива: ");  // приглашение ко вводу
+    array[i] = Convert.ToInt16(Console.ReadLine());
+}
+
+Console.Write("Наш массив: [" + string.Join(", ", array) + "]");
