27e72d40d90dd7d7f28eba51e09448985b9c948c
Nina <nzagorevskaia@gmail.com>
2022-08-24 15:54:13 +0300

    Add CS_Sem9_Task3 function of Ackermann

---
+++ CS_Seminar9/CS_Sem9_Task3/Program.cs
@@ -0,0 +1,27 @@
+﻿//Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+//Даны два неотрицательных числа m и n.
+
+//m = 2, n = 3 -> A(m,n) = 9
+
+int AckermanFunction(int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (m > 0 && n == 0) return AckermanFunction(m - 1, 1);
+    return AckermanFunction(m - 1, AckermanFunction(m, n - 1));
+}
+
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (m < 0 || n < 0)
+{
+    Console.Write("Функция не может быть вычислена: m и n не могут быть отрицательными.");
+}
+else
+{
+    int func = AckermanFunction(m, n);
+    Console.WriteLine(func);
+}
+
